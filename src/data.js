9e7e7b20ec8a046bb3e2d2159b220c9891a03a9f
AlineMussi <souzamussi@gmail.com>
2019-09-11 14:02:14 -0300

    teste2

--- src/data.js
+++ src/data.js
@@ -1,8 +1,23 @@
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+//filtro para um personagem especcífico
+function pesquisarPersonagem(){
+  let result = RICKANDMORTY.results.filter(character => character.name == "Rick Sanchez");
+
+  document.write("Retornou "+result[0].name+" personagens.");
+
+}
+//filtro para pesquisar por espécie
+function pesquisarEspecie(){
+  let resultSpecies = RICKANDMORTY.results.filter(character => character.species == "Human");
+
+  document.write("Retotnou "+resultSpecies.length+" personagens Humanos.");
+  
+} 
+
