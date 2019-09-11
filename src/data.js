8d73405bf7eb5ca459458765d8e2879396ee59b1
evs <evoraleite@gmail.com>
2019-09-11 15:43:21 -0300

    funções para busca de personagens sendo implementadas

--- src/data.js
+++ src/data.js
@@ -2,34 +2,41 @@
 // veja como agregamos a função ao objeto global window
 
 const example = () => {
+	
 	let arrayId = [];
 	let arrayName = [];
 	let arrayStatus = [];
-	arraySpecies = [];
-	arrayType = [];
-	arrayGender
-	arrayOrigin
-	arrayLocation
-	arrayImage
-	arrayEpisode
+	let arraySpecies = [];
+	// arrayType = [];
+	// arrayGender
+	// arrayOrigin
+	// arrayLocation
+	// arrayImage
+	// arrayEpisode
 
 	for (let i=0; i < RICKANDMORTY.results.length; i++) {
 		arrayId.push(RICKANDMORTY.results[i].id);
 		arrayName.push(RICKANDMORTY.results[i].name);
 		arrayStatus.push(RICKANDMORTY.results[i].status);
 		arraySpecies.push(RICKANDMORTY.results[i].species);
-		arrayType.pu
-		arrayGender
-		arrayOrigin
-		arrayLocation
-		arrayImage
-		arrayEpisode
+		// arrayType.pu
+		// arrayGender
+		// arrayOrigin
+		// arrayLocation
+		// arrayImage
+		// arrayEpisode
 		
 	}
-	return [arrayId[0], arrayName[0], arrayStatus[0]];
-	
+	return example2(arrayId, arrayStatus);
 	//return document.getElementById("test-here").innerHTML = (RICKANDMORTY.results.length);
-
 };
 
+const example2 = (arrayId, arrayStatus) => {
+	console.log(arrayId, arrayStatus);
+}
+	
+
+
+
 window.example = example;
+window.example2 = example2;
