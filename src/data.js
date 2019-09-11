3592ae26a3aaf39441e9dde5d5978a49d6bb66f7
evs <evoraleite@gmail.com>
2019-09-11 15:04:39 -0300

    funções para catar cada coisinha

--- src/data.js
+++ src/data.js
@@ -2,9 +2,34 @@
 // veja como agregamos a função ao objeto global window
 
 const example = () => {
-	e.preventDefault();
-	document.write(RICKANDMORTY.info);
-  
+	let arrayId = [];
+	let arrayName = [];
+	let arrayStatus = [];
+	arraySpecies = [];
+	arrayType = [];
+	arrayGender
+	arrayOrigin
+	arrayLocation
+	arrayImage
+	arrayEpisode
+
+	for (let i=0; i < RICKANDMORTY.results.length; i++) {
+		arrayId.push(RICKANDMORTY.results[i].id);
+		arrayName.push(RICKANDMORTY.results[i].name);
+		arrayStatus.push(RICKANDMORTY.results[i].status);
+		arraySpecies.push(RICKANDMORTY.results[i].species);
+		arrayType.pu
+		arrayGender
+		arrayOrigin
+		arrayLocation
+		arrayImage
+		arrayEpisode
+		
+	}
+	return [arrayId[0], arrayName[0], arrayStatus[0]];
+	
+	//return document.getElementById("test-here").innerHTML = (RICKANDMORTY.results.length);
+
 };
 
 window.example = example;
