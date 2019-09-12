aec6a89fe3766553f44fcb57fc67981ef85ff8b6
evs <evoraleite@gmail.com>
2019-09-12 16:56:43 -0300

    adding functions to show each char

--- src/data.js
+++ src/data.js
@@ -1,42 +1,44 @@
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
 
-const example = () => {
-	
-	let arrayId = [];
-	let arrayName = [];
-	let arrayStatus = [];
-	let arraySpecies = [];
-	// arrayType = [];
-	// arrayGender
-	// arrayOrigin
-	// arrayLocation
-	// arrayImage
-	// arrayEpisode
+
+
+
+const example = () => {	
+	document.getElementById("btn-char").addEventListener("click", example);
+	let inputChar = document.getElementById("char-name").value;	
+
+	let imageLoad = "";
+	let text = "";
 
 	for (let i=0; i < RICKANDMORTY.results.length; i++) {
-		arrayId.push(RICKANDMORTY.results[i].id);
-		arrayName.push(RICKANDMORTY.results[i].name);
-		arrayStatus.push(RICKANDMORTY.results[i].status);
-		arraySpecies.push(RICKANDMORTY.results[i].species);
-		// arrayType.pu
-		// arrayGender
-		// arrayOrigin
-		// arrayLocation
-		// arrayImage
-		// arrayEpisode
+		let name = RICKANDMORTY.results.filter(char => char.name);
+		let species = RICKANDMORTY.results.filter(char => char.species);
+		let status = RICKANDMORTY.results.filter(char => char.status);
+		let type = RICKANDMORTY.results.filter(char => char.type);
+		let gender = RICKANDMORTY.results.filter(char => char.gender);
+		let origin = RICKANDMORTY.results.filter(char => char.origin.name);
+		let location = RICKANDMORTY.results.filter(char => char.location.name);
+		let image = RICKANDMORTY.results.filter(char => char.image);
+
+		if (name[i].name === inputChar) {
+			let imageLoad = document.getElementById('test-image');
+			imageLoad.innerHTML = `<img src="` + image[i].image + `"/>`;
+
+			let text = document.getElementById('test-here-name').innerHTML += name[i].name + " " + species[i].species + " " + origin[i].origin.name;
+			return text + imageLoad
+		}
+
+		//imageLoad = document.getElementById('test-image').innerHTML += 
+		// text = document.getElementById('test-here-name').innerHTML += name[i].name + " " + species[i].species + " " + origin[i].origin.name + `<img src="` + image[i].image + `"/>`;
 		
-	}
-	return example2(arrayId, arrayStatus);
-	//return document.getElementById("test-here").innerHTML = (RICKANDMORTY.results.length);
+		
+	}	
+	// return text
 };
 
-const example2 = (arrayId, arrayStatus) => {
-	console.log(arrayId, arrayStatus);
-}
-	
-
-
+example();
 
 window.example = example;
-window.example2 = example2;
+
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// document.getElementById("btn-char").addEventListener("click", example);
+
+// const buttonCharSearch = () => {
+// 	event.preventDefault();
+
+// 	const inputChar = document.getElementById("char-name").value;
+
+// 	return example(inputChar);
+// }
+
