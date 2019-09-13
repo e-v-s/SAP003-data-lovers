b52c863e5f2e1b69b0be2df760b4fd832663f493
AlineMussi <souzamussi@gmail.com>
2019-09-13 01:29:27 -0300

    CSS e funcao atualizados

--- src/data.js
+++ src/data.js
@@ -7,10 +7,10 @@
 
 window.example = example;*/
 //filtro para um personagem especcífico
-function pesquisarPersonagem(){
-  let result = RICKANDMORTY.results.filter(character => character.name == "Rick Sanchez");
+/*function pesquisarPersonagem(){
+  let result = RICKANDMORTY.results.filter(character => character.name);
 
-  document.write("Retornou "+result[0].name+" personagens.");
+  document.write("Retornou "+result[1].name+" personagens.");
 
 }
 //filtro para pesquisar por espécie
@@ -19,5 +19,15 @@ function pesquisarEspecie(){
 
   document.write("Retotnou "+resultSpecies.length+" personagens Humanos.");
   
-} 
+}*/
+
+const personagem = RICKANDMORTY.results;
+
+for(let opcao of personagem){
+
+  const resultado = document.getElementById("resultado")
+
+  resultado.innerHTML += `${opcao.name} <img src="${opcao.image}"/></br>`
+
+}
 
