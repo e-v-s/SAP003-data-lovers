62fe41cf4b261f64ccc531ab901342873962c05a
AlineMussi <souzamussi@gmail.com>
2019-09-16 15:50:51 -0300

    repositorio Aline

--- src/data.js
+++ src/data.js
@@ -1,56 +1,3 @@
-// esta é uma função de exemplo
-// veja como agregamos a função ao objeto global window
-
-/*const example = () => {
-  return 'example';
-};
-
-window.example = example;*/
-//filtro para um personagem especcífico
-/*function pesquisarPersonagem(){
-  let result = RICKANDMORTY.results.filter(character => character.name);
-
-  document.write("Retornou "+result[1].name+" personagens.");
-
-}
-//filtro para pesquisar por espécie
-function pesquisarEspecie(){
-  let resultSpecies = RICKANDMORTY.results.filter(character => character.species == "Human");
-
-  document.write("Retotnou "+resultSpecies.length+" personagens Humanos.");
-  
-}*/
-
-/*const personagem = RICKANDMORTY.results;
-
-for(let opcao of personagem){
-
-  const resultado = document.getElementById("resultado")
-
-  resultado.innerHTML += `${opcao.name} <img src="${opcao.image}"/></br>`
-
-}*/
-
-/*let image ="";
-let name = "";
-let species ="";
-let origin ="";
-let image = "";
-
-const searchForCharacter = (inputChar, array) => {
-
-	for (let i of array) {
-		if (i.name.toLowerCase() === inputChar.toLowerCase()) {			
-			image = i.image;
-      name = i.name;
-      species = i.species;
-      origin = i.origin.name;				
-		}
-	return image, text;
-	}
-};*/
-
-
 const searchChar = () => {
   
   
@@ -83,8 +30,24 @@ for(let opcao of personagem){
 
 }
 
+document.getElementById("btn-clean").addEventListener("click", searchChar);
+  document.getElementById("char-name").value='';
+  sessionStorage.clear();
+
+
 
 /*window.data = {
 	searchForCharacter: searchForCharacter
 };*/
 
+//
+function pesquisarEspecie(){
+  let resultSpecies = RICKANDMORTY.results.filter(character => character.species === "Human");
+
+  for (let i of resultSpecies){
+    document.getElementById("char-name").innerHTML=i.species;     
+  }
+
+
+  //console.log(resultSpecies[0]);
+}
