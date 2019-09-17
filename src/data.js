befe5297adb54b8d14cf89c035bacc581a4563a3
evs <evoraleite@gmail.com>
2019-09-17 17:05:04 -0300

    n

--- src/data.js
+++ src/data.js
@@ -1,33 +1,50 @@
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
-
 //function to get the data from a specific char searched by the user
+const searchForCharacter = (inputChar, array, searchType) => {
 
-let image ="";
-let name = "";
-let species = "";
-let origin = "";
-let gender = "";
-let status = "";
-let type = "";
-let living = "";
-let answer = "";
-
-const searchForCharacter = (inputChar, array) => {
+	let arrayResult = [];
 
 	for (let i of array) {
-		if (i.name.toLowerCase() === inputChar.toLowerCase()) {			
-			image = i.image;
-			name = i.name;
-			species = i.species;
-			origin = i.origin.name;	
-			gender = i.gender;
-			status = i.status;
-			type = i.type;
-			living = i.location.name;			
+		switch (searchType) {
+			case "n":
+			if (i.name.toLowerCase() === inputChar.toLowerCase()) {
+				arrayResult.push(i);
+			} break;
+			case "g":
+				if(i.gender.toLowerCase() === inputChar.toLowerCase() ) {
+				arrayResult.push(i);
+			} break;
+			case "s":
+			if (i.species.toLowerCase() === inputChar.toLowerCase()) {
+				arrayResult.push(i);
+			} break;
+			case "st":
+			if (i.status.toLowerCase() === inputChar.toLowerCase()) {
+				arrayResult.push(i);
+			} break;
 		}
+	};
+
+
+	//creating new select option for gender
+
+
+	let character = "";
+
+	for(let i of arrayResult) {
+		character += `<div class="all-char">
+		<p class="all-name" id="char-name-search">${i.name}</p>
+		<p><img class="all-image" src="${i.image}"/></p>
+		<p class="all-gender"> Gender: ${i.gender}</p>
+		<p class="all-status"> Status: ${i.status}</p>
+		<p class="all-species"> Species: ${i.species}</p>
+		<p class="all-type"> Type: ${i.type}</p>
+		<p class="all-origin"> Origin: ${i.origin.name}</p>
+		<p class="all-location"> Location: ${i.location.name} </p>
+		</div>`;
 	}
-	return image, name, species, origin, gender, status, type, living;
+	return character;
 };
 
 //function to get every data of every character
@@ -50,16 +67,10 @@ const showAllChar = (array) => {
 	return imageAllChar;
 };
 
-const filterFunction = (valueOption) => {
-	
-
-	document.getElementById("test-here").innerHTML = valueOption.options[valueOption.selectedIndex].value;
-}
 
 window.data = {
 	searchForCharacter: searchForCharacter,
-	showAllChar: showAllChar,
-	filterFunction: filterFunction
+	showAllChar: showAllChar
 };
 
 
--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
 document.getElementById("btn-char").addEventListener("click", function() {
-		example();
+		searchCharacter();
 		cleanInputBox();
 });
 
 let array = RICKANDMORTY.results;
 
+//these create options on select tag
+const nameSelect = `<option value="n">${(Object.keys(array[0]))[1]}</option>`;
+const genderSelect = `<option value="g">${(Object.keys(array[0]))[5]}</option>`;
+const speciesSelect = `<option value="s">${(Object.keys(array[0]))[3]}</option>`;
+const statusSelect = `<option value="st">${(Object.keys(array[0]))[2]}</option>`;
+
+document.getElementById("search-type").innerHTML = nameSelect + genderSelect + speciesSelect + statusSelect;
+
 //Function for the search input
-const example = () => {		
+const searchCharacter = () => {
 
-	const inputChar = document.getElementById("char-name").value;
-	window.data.searchForCharacter(inputChar, array);
+	const inputChar = document.getElementById("char-value").value;
+	const searchType = document.getElementById("search-type").value;
 
-	document.getElementById("image").innerHTML = `<img src="${image}"/>`;
-	document.getElementById("name").innerHTML = name;
-	document.getElementById("species").innerHTML = species;
-	document.getElementById("origin").innerHTML = origin;
-	document.getElementById("gender").innerHTML = gender;
-	document.getElementById("status").innerHTML = status;
-	document.getElementById("type").innerHTML = type;
-	document.getElementById("living").innerHTML = living;
+	document.getElementById("show-images-of-all-char").innerHTML = window.data.searchForCharacter(inputChar, array, searchType);	
 };
 
 //function to show all characters at homepage
@@ -30,46 +31,13 @@ const showEverybody = () => {
 
 //function to clean input box
 const cleanInputBox = () => {
-	document.getElementById("char-name").value = "";
+	document.getElementById("char-value").value = "";
 }
-
-//function to filter characters
-// const filterFunction = (array) => {
-
-// 	for (let i of array) {
-// 		// let results = array.filter
-
-// 		console.log(i.species);
-// 	}	
-// }
-
-// filterFunction(array);
-
-//function to create options inside select tag
-const select = () => {
-	
-	const value = ["Selecione uma opção","gender", "species", "origin", "status", "type", "location"];
-	
-	for (let i of value) {
-		document.getElementById("select-by-properties").innerHTML += `<option value="${i}">${i}</option>`; 
-	}
-
-	document.getElementById("select-by-properties").addEventListener("change", filterFunction);
-}
-
-const valueOption = document.getElementById("select-by-properties");
-
-//function to test select() function. This must 
-// const fun = () => {
-	
-// 	document.getElementById("test-here").innerHTML = "opa";
-// }
 
 window.onload = function() {
-	example();
+	searchCharacter();
 	showEverybody();
 	cleanInputBox();
-	select();
 }
 
 
