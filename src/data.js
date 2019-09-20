039af88121b0900978430726fdc1599e8a3dfa19
evs <evoraleite@gmail.com>
2019-09-20 14:48:44 -0300

    added gitignore, editorconfig and eslintrc

--- src/data.js
+++ src/data.js
@@ -3,20 +3,20 @@
 //array is the object.results, which contains every character
 const searchForCharacter = (inputChar, array) => {
 	
-	let arrayResult = []
+  let arrayResult = [];
 
-	//includes every character inside arrayResults which includes the input from the user
-	for (let i of array) {
-		if (i.name.toLowerCase().includes(inputChar.toLowerCase())) {
-			arrayResult.push(i);
-		}
-	}
+  //includes every character inside arrayResults which includes the input from the user
+  for (let i of array) {
+    if (i.name.toLowerCase().includes(inputChar.toLowerCase())) {
+      arrayResult.push(i);
+    }
+  }
 
-	//show all char pushed to the arrayResult in cards
-	let character = "";
+  //show all char pushed to the arrayResult in cards
+  let character = "";
 
-	for(let i of arrayResult) {
-		character += `<div class="all-char">
+  for (let i of arrayResult) {
+    character += `<div class="all-char">
 		<p class="all-name" id="char-name-search">${i.name}</p>
 		<p><img class="all-image" src="${i.image}"/></p>
 		<p class="all-gender"> Gender: ${i.gender}</p>
@@ -26,26 +26,22 @@ const searchForCharacter = (inputChar, array) => {
 		<p class="all-origin"> Origin: ${i.origin.name}</p>
 		<p class="all-location"> Location: ${i.location.name} </p>
 		</div>`;
-	}
-	return character;
+  }
+  return character;
 };
 
 //search by radio button after selected option in select tag
-const searchByRadioButton = (buttonValue, array, searchType) =>  {
+const searchByRadioButton = (buttonValue, array, searchType) => {
 
-	let statusResult = "";
-	let genderResult = "";
-	let speciesResult = "";
-
-	let type = [];
+  let type = [];
 	
-		//first check the searchType chosen by the user in the select tag
-		if (searchType === "status") {
-			//then the result is filtered comparing the values inside the searchType with the buttonValue chosen by the user
-			statusResult = array.filter(i => i.status === buttonValue);
-			//just a for running through the result array to print the cards with the characers
-			for (let i of statusResult) {
-				type +=`<div class="all-char">
+  //first check the searchType chosen by the user in the select tag
+  if (searchType === "status") {
+    //then the result is filtered comparing the values inside the searchType with the buttonValue chosen by the user
+    let statusResult = array.filter(i => i.status === buttonValue);
+    //just a for running through the result array to print the cards with the characers
+    for (let i of statusResult) {
+      type +=`<div class="all-char">
 				<p class="all-name" id="char-name-search">${i.name}</p>
 				<p><img class="all-image" src="${i.image}"/></p>
 				<p class="all-gender"> Gender: ${i.gender}</p>
@@ -55,11 +51,11 @@ const searchByRadioButton = (buttonValue, array, searchType) =>  {
 				<p class="all-origin"> Origin: ${i.origin.name}</p>
 				<p class="all-location"> Location: ${i.location.name} </p>
 				</div>`;
-			}
-		} else if (searchType === "gender") {
-			genderResult = array.filter(i => i.gender === buttonValue);
-			for (let i of genderResult) {
-				type +=`<div class="all-char">
+    }
+  } else if (searchType === "gender") {
+    let genderResult = array.filter(i => i.gender === buttonValue);
+    for (let i of genderResult) {
+      type +=`<div class="all-char">
 				<p class="all-name" id="char-name-search">${i.name}</p>
 				<p><img class="all-image" src="${i.image}"/></p>
 				<p class="all-gender"> Gender: ${i.gender}</p>
@@ -69,11 +65,11 @@ const searchByRadioButton = (buttonValue, array, searchType) =>  {
 				<p class="all-origin"> Origin: ${i.origin.name}</p>
 				<p class="all-location"> Location: ${i.location.name} </p>
 				</div>`;
-			}
-		} else if (searchType === "species") {
-			speciesResult = array.filter(i => i.species === buttonValue);
-			for (let i of speciesResult) {
-				type +=`<div class="all-char">
+    }
+  } else if (searchType === "species") {
+    let speciesResult = array.filter(i => i.species === buttonValue);
+    for (let i of speciesResult) {
+      type +=`<div class="all-char">
 				<p class="all-name" id="char-name-search">${i.name}</p>
 				<p><img class="all-image" src="${i.image}"/></p>
 				<p class="all-gender"> Gender: ${i.gender}</p>
@@ -83,9 +79,9 @@ const searchByRadioButton = (buttonValue, array, searchType) =>  {
 				<p class="all-origin"> Origin: ${i.origin.name}</p>
 				<p class="all-location"> Location: ${i.location.name} </p>
 				</div>`;
-			}
-		}
-	return type;
+    }
+  }
+  return type;
 };
 
 //function to get every data of every character
@@ -93,8 +89,8 @@ let imageAllChar = "";
 
 const showAllChar = (array) => {
 
-	for(let i of array) {
-		imageAllChar += `<div class="all-char">
+  for (let i of array) {
+    imageAllChar += `<div class="all-char">
 		<p class="all-name" id="char-name-search">${i.name}</p>
 		<p><img class="all-image" src="${i.image}"/></p>
 		<p class="all-gender"> Gender: ${i.gender}</p>
@@ -104,14 +100,14 @@ const showAllChar = (array) => {
 		<p class="all-origin"> Origin: ${i.origin.name}</p>
 		<p class="all-location"> Location: ${i.location.name} </p>
 		</div>`;
-	}
-	return imageAllChar;
+  }
+  return imageAllChar;
 };
 
 //Dashboard functions
 
 window.data = {
-	searchForCharacter: searchForCharacter,
-	showAllChar: showAllChar,
-	searchByRadioButton: searchByRadioButton
+  searchForCharacter: searchForCharacter,
+  showAllChar: showAllChar,
+  searchByRadioButton: searchByRadioButton
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 document.getElementById("btn-char").addEventListener("click", function() {
-		searchCharacter();
-		cleanInputBox();
+  searchCharacter();
+  cleanInputBox();
 });
 
 //array created using the object.results
@@ -15,60 +15,59 @@ document.getElementById("search-type").innerHTML = genderSelect + speciesSelect 
 
 //Function used in searchForCharacter, used to search by name
 const searchCharacter = () => {
-	const inputChar = document.getElementById("char-value").value;
+  const inputChar = document.getElementById("char-value").value;
 
-	document.getElementById("show-images-of-all-char").innerHTML = window.data.searchForCharacter(inputChar, array);	
+  document.getElementById("show-images-of-all-char").innerHTML = window.data.searchForCharacter(inputChar, array);	
 };
 
 //function which creates radio buttons with the options related to the values inside each select tag (gender, status and species)
 const openRadioButton = () => {
-	document.getElementById("print-here").innerHTML = "";
+  document.getElementById("print-here").innerHTML = "";
 
-	const selectValue = document.getElementById("search-type").value;
+  const selectValue = document.getElementById("search-type").value;
 
-	const arrayMap = array.map(item => item[selectValue]);
+  const arrayMap = array.map(item => item[selectValue]);
 
-	let newArray = arrayMap.filter((item, index) => arrayMap.indexOf(item) === index);
+  let newArray = arrayMap.filter((item, index) => arrayMap.indexOf(item) === index);
 
-	document.getElementById("print-here").innerHTML += `${newArray.map(item => `<input class="checkbox" name="radio" type="radio" value="${item}">${item}</input>`)}`;
+  document.getElementById("print-here").innerHTML += `${newArray.map(item => `<input class="checkbox" name="radio" type="radio" value="${item}">${item}</input>`)}`;
 };
 
 //funtion which submit the values from the checkboxes created above to the searchRadioButton function
 const submit = () => {
-	event.preventDefault();
+  event.preventDefault();
 
-	//select tag value (gender, species or status)
-	const searchType = document.getElementById("search-type").value;
+  //select tag value (gender, species or status)
+  const searchType = document.getElementById("search-type").value;
 
-	//value from the radio button selected
-	buttonValue = document.querySelector(".checkbox:checked").value;
+  //value from the radio button selected
+  buttonValue = document.querySelector(".checkbox:checked").value;
 
-	document.getElementById("show-images-of-all-char").innerHTML = window.data.searchByRadioButton(buttonValue, array, searchType);
+  document.getElementById("show-images-of-all-char").innerHTML = window.data.searchByRadioButton(buttonValue, array, searchType);
 
-	//reseting the form after clicking search button
-	document.getElementById("form-search").reset();
-}
-
+  //reseting the form after clicking search button
+  document.getElementById("form-search").reset();
+};
 
 //function to show all characters at homepage by default
 const showEverybody = () => {
-	let showAllChar = window.data.showAllChar(array);
+  let showAllChar = window.data.showAllChar(array);
 
-	document.getElementById("show-images-of-all-char").innerHTML = imageAllChar;
-}
+  document.getElementById("show-images-of-all-char").innerHTML = imageAllChar;
+};
 
 //function to clean input box
 const cleanInputBox = () => {
-	document.getElementById("char-value").value = "";
-}
+  document.getElementById("char-value").value = "";
+};
 
 window.onload = function() {
-	searchCharacter();
-	showEverybody();
-	cleanInputBox();
-	openRadioButton();
-	submit();
-}
+  searchCharacter();
+  showEverybody();
+  cleanInputBox();
+  openRadioButton();
+  submit();
+};
 
 document.getElementById("search-type").addEventListener("change", openRadioButton);
 document.getElementById("btn-search-type").addEventListener("click", submit);
